344b06375b6deb9eabc6187be680fe68f0baacd6
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-03-28 22:45:54 -0500

    changed js and html

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon")
+  iconElement.innerHTML = `<img src"${response.data.condition.icon_url}" class="current-temperature-icon"/>`;
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
